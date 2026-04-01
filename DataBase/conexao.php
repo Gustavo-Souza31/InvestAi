5ddd431d61719a472266ab1d074c2af42dd84fe7
Gustavo-Souza31 <gustavo.souza8@pucpr.edu.br>
2026-04-01 09:49:34 -0300

    Implement MySQL database connection script with automatic port fallback

--- DataBase/conexao.php
+++ DataBase/conexao.php
@@ -4,14 +4,19 @@ $servername = "localhost";
 $usuario = "root";
 $senha = "";
 $banco = "investai";
+
+// Tenta porta 3306 primeiro, depois 3307 como reserva
 $porta = 3306;
-$porta = 3307;
+$conexao = @new mysqli($servername, $usuario, $senha, $banco, $porta);
 
-$conexao = new mysqli($servername, $usuario, $senha, $banco, $porta);
-
-// Checar conexão
 if ($conexao->connect_error) {
-    die("Conexão falhou: " . $conexao->connect_error);
+    $porta = 3307;
+    $conexao = new mysqli($servername, $usuario, $senha, $banco, $porta);
+}
+
+// Checar conexão final
+if ($conexao->connect_error) {
+    die("Conexão falhou em ambas as portas (3306 e 3307): " . $conexao->connect_error);
 }
 
 $conexao->set_charset("utf8mb4");
