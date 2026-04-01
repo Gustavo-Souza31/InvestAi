7a2b00fc9696de46a52d2dffa5a62e6806764fde
Gustavo-Souza31 <gustavo.souza8@pucpr.edu.br>
2026-04-01 09:52:20 -0300

    Add MySQL database connection script using mysqli

--- DataBase/conexao.php
+++ DataBase/conexao.php
@@ -4,19 +4,13 @@ $servername = "localhost";
 $usuario = "root";
 $senha = "";
 $banco = "investai";
-
-// Tenta porta 3306 primeiro, depois 3307 como reserva
 $porta = 3306;
-$conexao = @new mysqli($servername, $usuario, $senha, $banco, $porta);
 
-if ($conexao->connect_error) {
-    $porta = 3307;
-    $conexao = new mysqli($servername, $usuario, $senha, $banco, $porta);
-}
+$conexao = new mysqli($servername, $usuario, $senha, $banco, $porta);
 
-// Checar conexão final
+// Checar conexão
 if ($conexao->connect_error) {
-    die("Conexão falhou em ambas as portas (3306 e 3307): " . $conexao->connect_error);
+    die("Conexão falhou: " . $conexao->connect_error);
 }
 
 $conexao->set_charset("utf8mb4");
